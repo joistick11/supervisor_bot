85677f09eab3b28a03a9063eb2dee8938b2c8c32
don_faq <zhigalov8@gmail.com>
2016-10-12 13:00:00 +0300

    DB usage skeleton

--- bot.py
+++ bot.py
@@ -1,22 +1,47 @@
-import telepot
+import telebot as tb
 import settings
 import time
 
-
-# Адрес нашего бота: telegram.me/science_supervisor_bot
-
-def handle(msg):
-    flavor = telepot.flavor(msg)
-    summary = telepot.glance(msg, flavor=flavor)
-    print(flavor, summary)
+bot = tb.TeleBot(settings.token)
 
 
-TOKEN = settings.token
+def extract_unique_code(text):
+    """
+        Extracts the unique code from the sent '/start' command
+    """
+    return text.split()[1] if len(text.split()) > 1 else None
 
-bot = telepot.Bot(TOKEN)
-bot.message_loop(handle)
 
-print('Listening...')
+def in_storage(unique_code):
+    # TODO: Should check if unique code exists in storage
+    pass
 
-while 1:
-    time.sleep(10)
+
+def get_username_from_storage(unique_code):
+    # TODO: Does a query to the storage, retrieving the assosiated username
+    # FIXME: Should be replaced with real database lookup
+    return "ABC" if in_storage(unique_code) else None
+
+
+def save_chat_id(chat_id, username):
+    # TODO: Save chat_id->username to storage
+    pass
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    unique_code = extract_unique_code(message.text)
+    if unique_code:
+        username = get_username_from_storage(unique_code)
+        if username:  # if the username exists in our database
+            save_chat_id(message.chat_id, username)
+            reply = 'Hello {0}, how are you?'.format(username)
+        else:
+            reply = 'I have no clue who you are'
+    else:
+        reply = 'Please visit me via a provided URL from the website.'
+    bot.reply_to(message, reply)
+
+
+if __name__ == '__main__':
+    bot.polling()
