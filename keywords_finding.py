05393c40dc92d2e2aa0e3c369482458888489e59
gcinbax <gcinbax@gmail.com>
2016-10-25 22:55:27 +0200

    exclude minor parts of speech (prepositions, conjunctions, etc)

--- keywords_finding.py
+++ keywords_finding.py
@@ -6,7 +6,9 @@
 import requests as req
 import pymorphy2 as morph
 from collections import *
+import nltk
 from nltk import tokenize as tkn
+import nltk.corpus as crp
 
 
 # Accepts: file (https://core.telegram.org/bots/api/#file), bot token, n - Int
@@ -15,13 +17,15 @@ from nltk import tokenize as tkn
 # @bot.message_handler(content_types=['document'])
 # def handle_docs(message):
 #     keywords = find_keywords(bot.get_file(message.document.file_id), settings.keywords_finder_bot_token, 10)
-
+nltk.download("stopwords")
 def find_keywords(path, token, n):
     template = "https://api.telegram.org/file/bot{0}/{1}"
     file = req.get(template.format(token, path.file_path))
     text = file.text.encode("ISO-8859-1").decode("utf-8")
     tokenizer = tkn.RegexpTokenizer(r'\w+')
     tokens = tokenizer.tokenize(text)
+    stopwords = crp.stopwords.words('russian')
+    tokens = [i for i in tokens if ( i not in stopwords )]
     analyzer = morph.MorphAnalyzer()
     words = []
     for token in tokens:
