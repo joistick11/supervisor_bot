c6113559ad5a2d290ad84f84ef37cccc649211cc
gcinbax <gcinbax@gmail.com>
2016-10-26 11:26:12 +0200

    additionally exclude stopwords in normal form

--- keywords_finding.py
+++ keywords_finding.py
@@ -10,7 +10,6 @@ import nltk
 from nltk import tokenize as tkn
 import nltk.corpus as crp
 
-
 # Accepts: file (https://core.telegram.org/bots/api/#file), bot token, n - Int
 # Returns top n keywords
 # handler:
@@ -19,17 +18,21 @@ import nltk.corpus as crp
 #     keywords = find_keywords(bot.get_file(message.document.file_id), settings.keywords_finder_bot_token, 10)
 #     bot.send_message(message.chat.id, ', '.join(keywords))
 nltk.download("stopwords")
+
+
 def find_keywords(path, token, n):
     template = "https://api.telegram.org/file/bot{0}/{1}"
     file = req.get(template.format(token, path.file_path))
     text = file.text.encode("ISO-8859-1").decode("utf-8")
     tokenizer = tkn.RegexpTokenizer(r'\w+')
     tokens = tokenizer.tokenize(text)
-    stopwords = crp.stopwords.words('russian') + crp.stopwords.words('english')
-    tokens = [i for i in tokens if ( i not in stopwords )]
+    stopwords = crp.stopwords.words('russian') + crp.stopwords.words('english') + ['который']
+    tokens = [i for i in tokens if (i not in stopwords)]
     analyzer = morph.MorphAnalyzer()
     words = []
     for token in tokens:
-        words.append(analyzer.normal_forms(token)[0])
+        normal = analyzer.normal_forms(token)[0]
+        if normal not in stopwords:
+            words.append(normal)
     mosts = [word[0] for word in Counter(words).most_common(n)]
     return mosts
